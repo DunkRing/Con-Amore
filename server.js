6e75113e3cee7c29c2d8c2a662ade32755050e05
DunkRing <Chris31@live.dk>
2020-05-24 12:09:07 +0200

    old graph deleted

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 var dijkstra = require('./dijkstraPathAlgo.js');
-var graph = require('./graph.js');
 var stationer = require('./modelST.js');
 
-// console.log(dijkstra.data.findShortestPath(graph.graph, "A", "B"));// a-start-b 3
-// console.log(dijkstra.data.findShortestPath(graph.graph, "A", "start")); // a-start-b 3
-// console.log(dijkstra.data.findShortestPath(graph.graph, "start", "end")); // { distance: 8, path: [ 'start', 'A', 'D', 'end' ] }
-
-console.log(dijkstra.data.findShortestPath(stationer.stationer, "Hillerød", "KBHallen"));
+console.log(dijkstra.data.findShortestPath(stationer.stationer, "Hillerød", "Frederiksund"));
